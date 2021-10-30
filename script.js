eee4f83ad58c0072cef3f3eed9c813c7e9d37e8c
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2021-10-31 01:05:42 +0530

    Added prev & next control buttons

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ musicImg = wrapper.querySelector(".img-area img"),
 musicName = wrapper.querySelector(".song-details .name"),
 musicArtist = wrapper.querySelector(".song-details .artist"),
 mainAudio = wrapper.querySelector("#main-audio"),
-playPauseBtn = wrapper.querySelector(".play-pause");
+playPauseBtn = wrapper.querySelector(".play-pause"),
+prevBtn = wrapper.querySelector("#prev"),
+nextBtn = wrapper.querySelector("#next");
 
 let musicIndex = 2;
 
@@ -26,14 +28,36 @@ function playMusic()        //play music button function
     mainAudio.play();
 }
 
-function pauseMusic()      //pause misic button function
+function pauseMusic()      //pause music button function
 {
     wrapper.classList.remove("play");
     playPauseBtn.querySelector("i").innerText = "play_arrow";   //when clicked on pause button change to play button
     mainAudio.pause();
 }
 
-playPauseBtn.addEventListener("click",()=>{
+function nextMusic(){          //next music button function
+    musicIndex++;              //increment index by 1
+    musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;  //if musicIndex is greater than array length then musicIndex will be 1 so the first music will play
+    loadMusic(musicIndex);
+    playMusic();
+}
+
+function prevMusic(){          //prev music button function
+    musicIndex--;              //decrement index by 1
+    musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;  //if musicIndex is greater than 1 then musicIndex will be array length so the last music will play
+    loadMusic(musicIndex);
+    playMusic();
+}
+
+playPauseBtn.addEventListener("click",()=>{                  //play & pause button event
     const isMusicPaused = wrapper.classList.contains("paused");
     isMusicPaused ? pauseMusic() : playMusic();  //if music is paused call pause function else call play function
+});
+
+prevBtn.addEventListener("click",()=>{                //prev music button event
+    prevMusic();
+});
+
+nextBtn.addEventListener("click",()=>{               //next music button event
+    nextMusic();
 });
